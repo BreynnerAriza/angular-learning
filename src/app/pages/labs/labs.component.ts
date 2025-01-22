d3d64bbe097cb2754f3b8fe4af14026eab008f28
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2025-01-22 11:52:31 -0500

    Agregando signals al proyecto

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -14,7 +14,7 @@ export class LabsComponent {
     'Crear un nuevo proyecto',
     'Imprimir un arreglo'
   ];
-  name = 'Breynner Ariza';
+  nombre = 'Breynner Ariza';
   edad = 18;
 
   // PROPERTI BINDING
@@ -23,5 +23,30 @@ export class LabsComponent {
   imgDescription = 'Foto de perfil';
   buttomValue = 'Click me';
   buttomDisabled = true;
+
+  //Creacion de objetos
+  person = {
+    name: 'Breynner Ariza',
+    age: 21,
+    description: 'Utilizando propiedades utilizando un objeto'
+  }
+
+  //NAME REACTIVO
+  nameReactivo = signal('Breynner Ariza Reactivo');
+
+  clickHander(){
+    alert('Hola');
+  }
+
+  chageInput(event : Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.nameReactivo.set(newValue);
+  }
+
+  keyDownHandler(event : KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 
 }
