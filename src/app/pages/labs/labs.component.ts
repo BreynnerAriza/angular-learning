9a12cf288089ab7cbd9081b328d875efdbdf5103
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2025-01-22 10:15:52 -0500

    Creacion del router y plantilla basica del to do

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-labs',
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
-  title = 'todoapp';
+export class LabsComponent {
   saludoVariable = 'Hola desde la logica';
   tasks = [
     'Instalar angular',
